0fbba5c0dc2db6d177b77c129f3a92b701bba153
yash <yashvikram8250@gmail.com>
2024-12-29 19:05:50 +0530

    backend

---
+++ api/src/index.ts
@@ -0,0 +1,14 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({port: 8080});
+
+wss.on("connection",function(socket){
+    console.log("user connected");
+
+    socket.on("message",(e)=>{
+        if(e.toString()==="ping"){
+            socket.send("pong");
+        }
+        else socket.send("wrong message");
+    })
+})
